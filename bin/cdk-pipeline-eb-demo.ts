5c083899b766c20564cd4fb527bb6d9f9dcf04b4
Jason Tan <jtan20040401@gmail.com>
2023-06-20 16:36:31 +1000

    Fix region

--- bin/cdk-pipeline-eb-demo.ts
+++ bin/cdk-pipeline-eb-demo.ts
@@ -15,7 +15,7 @@ new CdkPipelineStack(app, 'CdkPipelineStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  env: { account: '772089055204', region: 'app-southeast-2' },
+  env: { account: '772089055204', region: 'ap-southeast-2' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
